62887ff53761a62cccb0434a286b5ca32137f915
denosoft2020 <hemasafari2017@gmail.com>
2025-10-26 21:30:39 +0400

    some last steps for web version

--- backend/election/settings.py
+++ backend/election/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ucsa^xnl+-obxk)&x#9p+_yg$9oixt&3nara8#bn6bte)rnjh2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
